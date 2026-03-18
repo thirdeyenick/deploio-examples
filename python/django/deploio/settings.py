32c10a27a639407b9900b6f3b10a8cedf5166cfe
Sebastian Nickel <nick@nine.ch>
2026-03-18 15:16:18 +0100

    trust the deploio domain for CSRF

--- python/django/deploio/settings.py
+++ python/django/deploio/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.deploio.app",
+]
